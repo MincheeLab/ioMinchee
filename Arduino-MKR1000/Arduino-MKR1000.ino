2d0feaf27db1386eeb801da464687de3ac3d7fc2
Gerald Estadieu <gestadieu@gmail.com>
2016-05-11 09:46:46 +0800

    renaming

--- Arduino-MKR1000/Arduino-MKR1000.ino
+++ Arduino-MKR1000/Arduino-MKR1000.ino
@@ -8,11 +8,16 @@
 boolean _debug = true;
 
 // Inital AP & WiFi
-char ssid[] = "lightme";
-char pass[] = "lightme";
+char ssid[] = "Minchee-IoT";
+char pass[] = "Minchee-IoT";
 int wifi_status = WL_IDLE_STATUS;
+WiFiServer http_server(80);
 WiFiClient wifi;
 //WiFiSSLClient wifi;
+// mdns config
+//char mdnsName[] = "IoTMinchee";
+//WiFiMDNSResponder mdnsResponder;
+
 
 // Flash storage
 typedef struct{
@@ -26,7 +31,7 @@ Settings settings;
 
 // MQTT
 MQTTClient mqtt;
-const char broker[] = "172.24.14.79";
+const char broker[] = "192.168.2.118";
 // temporary
 const char deviceId[]   = "11214bf3-3ef8-4635-b2d8-19fc3791a4b9";
 const char devicePsw[]  = "b049f0ad-d9af-4e9b-a2ef-cdff2b29d61e";
@@ -37,24 +42,70 @@ void setup() {
     SERIAL_PORT_MONITOR.begin(9600);
     while (!SERIAL_PORT_MONITOR) { }
     Serial.println("rebooting");
-    saveSettings();
+    //    saveSettings();
   }
-
+  
   getSettings();
   if (!settings.valid) {
     ap();
+    settingsPage();
   } else {
     connect();
   }
-
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!settings.valid) {
+    WiFiClient client;
+    client = http_server.available();
+    
+    if (client) {                             // if you get a client,
+      Serial.println("new client");           // print a message out the serial port
+      String currentLine = "";                // make a String to hold incoming data from the client
+      while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+      char c = client.read();             // read a byte, then
+      Serial.write(c);                    // print it out the serial monitor
+      if (c == '\n') {                    // if the byte is a newline character
+        
+        // if the current line is blank, you got two newline characters in a row.
+        // that's the end of the client HTTP request, so send a response:
+        if (currentLine.length() == 0) {
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-type:text/html");
+          client.println();
+          client.print("<form><input type=\"text\" name=\"ssid\"><br/><input type=\"password\" name=\"pwd\"></form>");
+          client.println();
+          break;
+        }
+        else {      // if you got a newline, then clear currentLine:
+          currentLine = "";
+        }
+      }
+      else if (c != '\r') {    // if you got anything else but a carriage return character,
+        currentLine += c;      // add it to the end of the currentLine
+      }
+      
+      // Check to see if the client request was "GET /H" or "GET /L":
+      //        if (currentLine.endsWith("GET /H")) {
+      //          digitalWrite(led, HIGH);               // GET /H turns the LED on
+      //        }
+      //        if (currentLine.endsWith("GET /L")) {
+      //          digitalWrite(led, LOW);                // GET /L turns the LED off
+      //        }
+    }
+  }
+  // close the connection:
+  client.stop();
+}
+//process_http_request();
+} else {
   mqtt.loop();
   if(!mqtt.connected()){
     connect();
-  }
+  }  
+}
 }
 
 void ap() {
@@ -68,9 +119,21 @@ void ap() {
   }
 }
 
+void settingsPage() {
+  http_server.begin();
+  if (_debug) {
+    Serial.println("HTTP server started...");
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+    printMacAddress();
+  }
+  
+}
+
 void saveSettings() {
-  String ssid = "USJ.Staff";
-  String pwd = "usjwirelessstaff001";
+  String ssid = "HomeSweetHome";
+  String pwd = "geraldestadieu";
   String deviceName = "mkr1000";
   settings.valid = true;
   ssid.toCharArray(settings.ssid, 20);
@@ -111,7 +174,7 @@ void connect() {
     Serial.println(ip);
     printMacAddress();
   }
-
+  
   mqtt.begin(broker, wifi);
   while(!mqtt.connect(settings.deviceName, deviceId, devicePsw)) {
     delay(1000);
@@ -139,7 +202,7 @@ void messageReceived(String topic, String payload, char * bytes, unsigned int le
 void printMacAddress() {
   // the MAC address of your Wifi shield
   byte mac[6];
-
+  
   // print your MAC address:
   WiFi.macAddress(mac);
   Serial.print("MAC: ");
@@ -183,4 +246,5 @@ void printMacAddress() {
 //    Serial.flush();
 //  }
 //}
+
 
